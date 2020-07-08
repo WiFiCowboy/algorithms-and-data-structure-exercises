426240eeb7f2d24c120a961711f92ae3e446bae8
Humberto Hernandez <hamburger45@gmail.com>
2020-07-07 21:07:20 -0700

    completed 4-7

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,14 +9,9 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  // helpers parsInt = convert to number, toString = converto to string, Math.sign returns - or + only
   const reversed = n.toString().split("").reverse().join("");
 
   return parseInt(reversed) * Math.sign(n);
-  // const reversed = parseInt(
-  //   Math.sign(n.toString().split("").reverse().join(""))
-  // );
-  // return reversed;
 }
 
 module.exports = reverseInt;
